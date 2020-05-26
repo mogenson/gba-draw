cf70c85347dfdb8d1f15f22c7a6c112e77b0adac
Michael Mogenson <michael.mogenson@gmail.com>
2020-05-26 18:20:06 -0400

    Add DrawTarget trait for Tile8bpp

--- src/gba_display.rs
+++ src/gba_display.rs
@@ -1,34 +1,17 @@
-use core::convert::Infallible;
-use embedded_graphics::{drawable::Pixel, geometry::Size, pixelcolor::Bgr555, prelude::*};
+use core::convert::{Infallible, TryInto};
+use embedded_graphics::{
+    drawable::Pixel,
+    geometry::Size,
+    pixelcolor::{raw::RawU8, Bgr555, PixelColor},
+    prelude::*,
+};
 use gba::{
-    io::display::{DisplayControlSetting, DisplayMode, DISPCNT},
-    vram::bitmap::Mode3,
+    vram::{bitmap::Mode3, Tile8bpp},
     Color,
 };
 
 /// Empty struct representing GBA Display
 pub struct GbaDisplay;
-
-impl GbaDisplay {
-    /// Creates a Mode3 GBA Display
-    pub fn new() -> Self {
-        DISPCNT.write(
-            DisplayControlSetting::new()
-                .with_mode(DisplayMode::Mode3)
-                .with_bg2(true),
-        );
-
-        GbaDisplay
-    }
-
-    pub const fn width() -> u32 {
-        Mode3::WIDTH as u32
-    }
-
-    pub const fn height() -> u32 {
-        Mode3::HEIGHT as u32
-    }
-}
 
 impl DrawTarget<Bgr555> for GbaDisplay {
     type Error = Infallible;
@@ -52,5 +35,53 @@ impl DrawTarget<Bgr555> for GbaDisplay {
     fn clear(&mut self, color: Bgr555) -> Result<(), Self::Error> {
         Mode3::dma_clear_to(Color(color.into_storage()));
         Ok(())
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct PaletteColor(RawU8);
+
+impl PaletteColor {
+    /// Create a new color from palette entry index
+    pub const fn new(index: u8) -> Self {
+        Self(RawU8::new(index))
+    }
+
+    pub const TANSPARENT: Self = Self(RawU8::new(0));
+}
+
+impl PixelColor for PaletteColor {
+    type Raw = RawU8;
+}
+
+impl From<RawU8> for PaletteColor {
+    fn from(data: RawU8) -> Self {
+        Self(data)
+    }
+}
+
+impl From<PaletteColor> for RawU8 {
+    fn from(value: PaletteColor) -> Self {
+        value.0
+    }
+}
+
+impl DrawTarget<PaletteColor> for Tile8bpp {
+    type Error = Infallible;
+
+    /// Draw a `pixel` that has a color defined as `Bgr555`
+    fn draw_pixel(&mut self, pixel: Pixel<PaletteColor>) -> Result<(), Self::Error> {
+        if let Ok((x @ 0..8, y @ 0..8)) = pixel.0.try_into() {
+            let index: u32 = x + (y * 8); // index into [u8; 64] array
+            let word: &mut u32 = &mut self.0[index as usize / 4];
+            *word &= !(0xFF << ((index % 4) * 8)); // clear byte
+            *word |= (pixel.1.into_storage() as u32) << ((index % 4) * 8); // set byte
+        }
+        Ok(())
+    }
+
+    /// Return size of drawable display
+    fn size(&self) -> Size {
+        Size::new(8, 8)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,34 @@
 #![no_std]
 #![feature(start)]
 #![forbid(unsafe_code)]
+#![feature(exclusive_range_pattern)]
+#![feature(bindings_after_at)]
 
 mod gba_display;
-use gba_display::GbaDisplay;
+use gba_display::{GbaDisplay, PaletteColor};
 
-use core::convert::{TryFrom, TryInto};
+use core::convert::{Infallible, TryFrom, TryInto};
 
 use embedded_graphics::{
     fonts::{Font12x16, Text},
     image::Image,
     pixelcolor::Bgr555,
     prelude::*,
-    primitives::{Circle, Rectangle},
+    primitives::{Circle, Line, Rectangle},
     style::{PrimitiveStyle, TextStyle},
 };
 
 use gba::{
     debug, fatal,
     io::{
-        display::{DisplayStatusSetting, DISPSTAT},
+        display::{DisplayControlSetting, DisplayMode, DisplayStatusSetting, DISPCNT, DISPSTAT},
         irq::{set_irq_handler, IrqEnableSetting, IrqFlags, BIOS_IF, IE, IF, IME},
         keypad::read_key_input,
     },
+    oam::{write_obj_attributes, OBJAttr0, OBJAttr1, OBJAttr2, ObjectAttributes},
+    palram::index_palram_obj_8bpp,
+    vram::{bitmap::Mode3, get_8bpp_character_block, Tile8bpp},
+    Color,
 };
 
 use tinytga::Tga;
@@ -35,22 +41,40 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
 
 #[start]
 fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    debug!("Creating EG display");
-    let mut display = GbaDisplay::new();
+    debug!("Set up display");
+    DISPCNT.write(
+        DisplayControlSetting::new()
+            .with_mode(DisplayMode::Mode3) // bitmap
+            .with_bg2(true) // use background
+            .with_obj(true) // use sprites
+            .with_oam_memory_1d(true) // 1 dimensional vram mapping
+            .with_force_vblank(true), // disable display
+    );
+
+    debug!("Register palette");
+    register_palette();
+
+    debug!("Draw reticle");
+    draw_reticle().ok();
+
+    debug!("Create display");
+    let mut display = GbaDisplay;
     draw_tga(&mut display).ok();
     draw_text(&mut display).ok();
 
-    debug!("Enabling interrupts");
+    debug!("Enable interrupts");
     set_irq_handler(irq_handler);
     DISPSTAT.write(DisplayStatusSetting::new().with_vblank_irq_enable(true));
     IE.write(IrqFlags::new().with_vblank(true));
     IME.write(IrqEnableSetting::IRQ_YES);
 
-    const WIDTH: u32 = GbaDisplay::width();
-    const HEIGHT: u32 = GbaDisplay::height();
+    const WIDTH: u32 = Mode3::WIDTH as u32;
+    const HEIGHT: u32 = Mode3::HEIGHT as u32;
     let mut point = Point::try_from((WIDTH, HEIGHT)).unwrap() / 2;
 
-    debug!("Starting main loop");
+    debug!("Start main loop");
+    DISPCNT.write(DISPCNT.read().with_force_vblank(false)); // enable display
+
     loop {
         // sleep until vblank interrupt
         gba::bios::vblank_interrupt_wait();
@@ -59,17 +83,18 @@ fn main(_argc: isize, _argv: *const *const u8) -> isize {
         let input = read_key_input();
 
         // adjust game state and wait for vblank
-        point += Point::new(input.x_tribool() as i32, input.y_tribool() as i32);
+        let offset = Point::new(input.x_tribool() as i32, input.y_tribool() as i32);
+        point += offset;
 
-        if let Ok((0..=WIDTH, 0..=HEIGHT)) = point.try_into() {
-            Circle::new(point, 1)
-                .into_styled(PrimitiveStyle::with_fill(Bgr555::CYAN))
-                .draw(&mut display)
-                .ok();
+        if let Ok((x @ 0..WIDTH, y @ 0..HEIGHT)) = point.try_into() {
+            move_reticle(x as u16, y as u16);
+            if input.a() {
+                Pixel(Point::new(x as i32, y as i32), Bgr555::BLUE)
+                    .draw(&mut display)
+                    .ok();
+            }
         } else {
-            draw_tga(&mut display).ok();
-            draw_text(&mut display).ok();
-            point = Point::try_from((WIDTH, HEIGHT)).unwrap() / 2;
+            point -= offset; // undo
         }
     }
 }
@@ -81,14 +106,14 @@ extern "C" fn irq_handler(flags: IrqFlags) {
     }
 }
 
-fn draw_tga(display: &mut GbaDisplay) -> Result<(), core::convert::Infallible> {
+fn draw_tga(display: &mut GbaDisplay) -> Result<(), Infallible> {
     let tga = Tga::from_slice(include_bytes!("../assets/amy.tga")).unwrap();
     let image: Image<Tga, Bgr555> = Image::new(&tga, Point::zero());
     image.draw(display)?;
     Ok(())
 }
 
-fn draw_text(display: &mut GbaDisplay) -> Result<(), core::convert::Infallible> {
+fn draw_text(display: &mut GbaDisplay) -> Result<(), Infallible> {
     Text::new("Dirty Fucking Amy", Point::new(20, 20))
         .into_styled(TextStyle::new(Font12x16, Bgr555::CYAN))
         .draw(display)?;
@@ -96,4 +121,50 @@ fn draw_text(display: &mut GbaDisplay) -> Result<(), core::convert::Infallible> 
         .into_styled(PrimitiveStyle::with_stroke(Bgr555::CYAN, 3))
         .draw(display)?;
     Ok(())
+}
+
+fn register_palette() {
+    // slot 0 is for transparency
+    index_palram_obj_8bpp(1).write(Color(Bgr555::BLACK.into_storage()));
+    index_palram_obj_8bpp(2).write(Color(Bgr555::RED.into_storage()));
+    index_palram_obj_8bpp(3).write(Color(Bgr555::GREEN.into_storage()));
+    index_palram_obj_8bpp(4).write(Color(Bgr555::BLUE.into_storage()));
+    index_palram_obj_8bpp(5).write(Color(Bgr555::YELLOW.into_storage()));
+    index_palram_obj_8bpp(6).write(Color(Bgr555::MAGENTA.into_storage()));
+    index_palram_obj_8bpp(7).write(Color(Bgr555::CYAN.into_storage()));
+    index_palram_obj_8bpp(8).write(Color(Bgr555::WHITE.into_storage()));
+}
+
+fn draw_reticle() -> Result<(), Infallible> {
+    let mut tile = Tile8bpp([PaletteColor::TANSPARENT.into_storage().into(); 16]);
+    let style = PrimitiveStyle::with_stroke(PaletteColor::new(4), 1);
+
+    Circle::new(Point::new(3, 3), 3)
+        .into_styled(style)
+        .draw(&mut tile)?;
+
+    Line::new(Point::new(3, 0), Point::new(3, 6))
+        .into_styled(style)
+        .draw(&mut tile)?;
+
+    Line::new(Point::new(0, 3), Point::new(6, 3))
+        .into_styled(style)
+        .draw(&mut tile)?;
+
+    get_8bpp_character_block(5).index(1).write(tile);
+
+    Ok(())
+}
+
+fn move_reticle(x: u16, y: u16) {
+    write_obj_attributes(
+        0,
+        ObjectAttributes {
+            attr0: OBJAttr0::new()
+                .with_row_coordinate(y - 3)
+                .with_is_8bpp(true),
+            attr1: OBJAttr1::new().with_col_coordinate(x - 3),
+            attr2: OBJAttr2::new().with_tile_id(514), // 8bpp tiles are even offset
+        },
+    );
 }
